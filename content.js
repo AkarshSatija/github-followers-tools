7fb30e7fc450e137184dec8bc618877a61609a48
Akarsh <akarsh.satija@gmail.com>
2017-02-04 21:21:01 +0530

    Setting up plus permissions + options page etc

--- content.js
+++ content.js
@@ -1,4 +1,19 @@
 debugger;
+
+var me='akarshsatija';
+
 var list=$('.d-table.col-12.width-full.py-4.border-bottom.border-gray-light > .pr-3 > .mb-1 ');
 
 list.append('<span> [Follows You]</span>');
+
+window.onpopstate=function(){
+	console.log("state changed")
+}
+
+
+// get number of following https://api.github.com/users/akarshsatija
+//get all following
+// save in DB or local storage
+
+
+$.get('https://api.github.com/users/'+me+'/following?per_page=100')
